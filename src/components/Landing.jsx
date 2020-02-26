124e77d68261d89675b7b904940a6ac1c98b80fa
Jesse Zuniga <jzuniga206@gmail.com>
2020-02-26 15:24:22 -0800

    favorites logic complete. all user favorite camps load on succcessful login. styling in progress.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,7 @@
 import React, { Component, useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
-import Favorite from './Favorite';
+import { Favorite } from './Favorite';
 
 /* The Landing page is the page our users are presented with on login or signup. Here, we 
 meant to render our current favorites in cards that might provide additional details for the campsites 
@@ -11,35 +11,39 @@ potential campgrounds via the Active API. */
 const landing = props => {
   const { hasFavs } = props;
   const [favs, setFavs] = useState([]);
+  const [gotData, setGotData] = useState(false); // prevent re-running of useEffect
   let userId; //capture userId here from state for fetch below
 
-  // useEffect(() => {
-  //   // upon load, the landing page will query back end for a specific users fav camps
-  //   // if hasFavs was not defined during login from the App component, nothing will be queried
-  //   if (hasFavs) {
-  //     fetch('/user/favorites/1') //update 1 to userId dynamically later
-  //       .then(res => res.json)
-  //       .then(data => setFavs(data))
-  //       .catch(error => console.log(error));
-  //   }
-  // }, []); // empty array keeps useEffect from running indefinitely
+  useEffect(() => {
+    // upon load, the landing page will query back end for a specific users fav camps
+    // if hasFavs was not defined during login from the App component, nothing will be queried
+    if (hasFavs) {
+      if (gotData === false) {
+        // this boolean prevents the query from running more than once
+        fetch('/user/favorites/1') //update 1 to userId dynamically later
+          .then(res => res.json())
+          .then(data => {
+            setGotData(true);
+            setFavs(data.user);
+          })
+          .catch(error => console.log('useEffect error:', error));
+      }
+    }
+  }, []); // empty array keeps useEffect from running indefinitely, useEffect runs whenever favs is updated
 
-  let favsheader;
-
-  if (hasFavs === true) {
-    favsheader = <h3>Your Favorites</h3>;
-  }
+  // create array of Favorite components for every favorite returned from query into favs state
+  const favoriteComponents = favs.map((favorite, idx) => {
+    return <Favorite data={favorite} key={idx} />;
+  });
 
   return (
-    <div className='Landing'>
-      <h1>Let's Go Camping</h1>
+    <div className='Landing' style={{ textAlign: 'center' }}>
+      <h1>fireside</h1>
       <Link to='/camp'>
-        <Button size='large' outline color='primary'>
-          Find Camps
-        </Button>
+        <Button color='primary'>Find Camps</Button>
       </Link>
-      {favsheader}
-      {favs}
+      {gotData ? <h3 className='favsHeader'>Your Favorites</h3> : ''}
+      <div>{favoriteComponents}</div>
     </div>
   );
 };
